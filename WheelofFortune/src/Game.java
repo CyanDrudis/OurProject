55b440e29913e477b5ce8a72421da98466f66213
CyanDrudis <52932421+CyanDrudis@users.noreply.github.com>
2019-07-19 11:12:47 -0600

    Add files via upload

--- WheelofFortune/src/Game.java
+++ WheelofFortune/src/Game.java
@@ -1,5 +1,32 @@
-
+import java.util.ArrayList;
 public class Game 
 {
-
+	private String puzzle;
+	private String answer;
+	private ArrayList<Boolean> turns = new ArrayList<Boolean>();
+	private ArrayList<String> wheel = new ArrayList<String>();
+	public void bankrupt(Player p) 
+	{
+		//p.setMoney(0.00);
+	}
+	public void loseATurn(Player p)
+	{
+		//p.skipTurn(p)
+	}
+	public void freePlay(Player p) 
+	{
+		//p.pricePerVowel(0, 1)
+	}
+	public void spin() 
+	{
+		
+	}
+	public boolean checkAns() 
+	{
+		return true;
+	}
+	public void input(char a) 
+	{
+		
+	}
 }
