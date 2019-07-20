c7bc22dee03dcd349a9c0eb6a4efc6bc0dcf568b
RoiCB7 <cbbcalgary@hotmail.com>
2019-07-20 13:45:17 -0600

    added modified Puzzle.java

--- WheelofFortune/src/Puzzle.java
+++ WheelofFortune/src/Puzzle.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
 
-public class Puzzle 
+public class Puzzle extends Game
 {
-//hello world!
+	/*
+	 * Possible constructors.
+	 * 
+	 */
+	
+	
+private String puzzle;
+private String answer;
+private ArrayList<String> wheel = new ArrayList <String>();
+
+
+public String getPuzzle() {
+	return puzzle;	
+}
+
+public String getAnswer () {
+	return answer;
+}
+
+public ArrayList<String> getWheel () {
+	return wheel;
+}
+
+
+
+
+
+
+
 }
