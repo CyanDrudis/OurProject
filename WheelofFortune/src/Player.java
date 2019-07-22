957dd1ce1c51b119a67c681c157a83ea7c56ea75
RoiCB7 <16857308+RoiCB7@users.noreply.github.com>
2019-07-22 16:12:52 -0600

    Add files via upload
    -added constants 
    -added documentation

--- WheelofFortune/src/Player.java
+++ WheelofFortune/src/Player.java
@@ -1,45 +1,144 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
+/*********************************************************************************************************************
+ * CLASS: Player
+ * 
+ * PURPOSE: To pass, set and modify relevant information about the player as the game goes on. 
+ *          This is represented as a players 'account' (the amount of money they have), their name and the prize list attached to them.
+ * 
+ * PRIVATE VARIABLES: -name: a String 
+ *                    -money: a double, the player's 'account' so to say  
+ *                    -bonus:   
+ *                    -vowelCost:
+ *                    -prizeList: 
+ *                    
+ * NOTES: Negative balance is possible and allowed in Wheel of Fortune, therefore it is not accounted for here.
+ *********************************************************************************************************************/
 
 public class Player extends Game{
+	private static final int bonus =5;
+	private static final double vowelCost = 250;
 	private String name;
-	private double money;
-	private ArrayList<Boolean> prizeList = new ArrayList<>(Collections.nCopies(5, false));
+	private double money; //Player's account balance so to say
+	private ArrayList<Boolean> prizeList = new ArrayList<>(Collections.nCopies(bonus, false));
 	
 	//[0] = cruise, [1] = wildcard, [2] = 1/2 car (1st half) [3] = 1/2 car (2nd half)
 	//[4] = 1 million dollar prize
+	
+	/* FUNCTION: getPrizeListBoolean
+	 * 
+	 * PURPOSE: 
+	 * 
+	 * METHOD: 
+	 * 
+	 * RETURNS: a Boolean
+	 *
+	 * INPUT PARAMETERS:  an integer
+	 */
 	public Boolean getPrizeListBoolean(int index) {
 		return prizeList.get(index);
 	}
 	
+	/************************************************************************
+	 * FUNCTION: getMoney
+	 * 
+	 * PURPOSE: returns the amount of money in a player's account
+	 * 
+	 * METHOD: return instance variable money
+	 * 
+	 * RETURNS: a double
+	 ************************************************************************/
 	public double getMoney() {
 		return money;
 	}
-
+	
+    /************************************************************************
+	 * FUNCTION: getName
+	 * 
+	 * PURPOSE: return the given name of the player
+	 * 
+	 * METHOD: return instance variable name
+	 * 
+	 * RETURNS: a String
+	 ************************************************************************/
 	public String getName() {
 		return name;
 	}
 	
+	/************************************************************************
+	 * FUNCTION: setMoney
+	 * 
+	 * PURPOSE: set the amount of money for the invoking object/instance
+	 * 
+	 * METHOD: make the instances' money variable equal to what is passed in
+	 * 
+	 * RETURNS: nothing.
+	 * 
+	 * INPUT PARAMETERS:  a double
+	 ************************************************************************/
 	public void setMoney(double money) {
 		this.money = money;
 	}
 	
+	/************************************************************************
+	 * Function: setName 
+	 * 
+	 * Purpose: set the given name for the invoking object/instance
+	 * 
+	 * Method: makes the object's name variable equal to what is passed in
+	 * 
+	 * Returns: nothing
+	 * 
+	 * Input Parameters: a String
+	 ************************************************************************/
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/*************************************************************************
+	 * Function: deposit
+	 * 
+	 * Purpose:  deposit a certain amount into the player's money, a placeholder for some type of account
+	 * 
+	 * Method: add the players money by the amount passed in
+	 * 
+	 * Returns: nothing
+	 * 
+	 * Input Parameters: a double 
+	 *************************************************************************/
 	public void deposit(double amount) {
 		this.money = this.money + amount;
 	}
 	
+	/*************************************************************************
+	 * Function: withdraw 
+	 * 
+	 * Purpose: take out a certain amount of money from the players account.
+	 * 
+	 * Method: subtract the money in a player's account by the value passed in.
+	 * 		   note: It is possible to have a negative balance in Wheel of Fortune so this is not accounted for here.	
+	 * 
+	 * Returns: nothing
+	 * 
+	 * Input Parameters: a double 
+	 **************************************************************************/
 	public void withdraw(double amount) {
 		this.money = this.money - amount;
 	}
 	
+	/****************************************************************************
+	 * Function: pricePerVowel
+	 * 
+	 * Purpose: the player has to pay in order to use a vowel //Better description later 
+	 * 
+	 * Method: the player's account is subtracted by the number of vowels used multiples by the cost which is 250.
+	 * 
+	 * Returns: nothing
+	 * 
+	 * Input Parameters: an integer 
+	 ******************************************************************************/
 	public void pricePerVowel(int numberOfVowels) {
-		this.money = this.money - numberOfVowels*250;
+		this.money = this.money - numberOfVowels*vowelCost; //Do we need to subtract this.money as well? This will always result in a negative num
 	}
-	
-	 
 }
