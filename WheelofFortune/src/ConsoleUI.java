c13df9f93f31649b40c4898eb0857cd4fb609173
RoiCB7 <16857308+RoiCB7@users.noreply.github.com>
2019-07-22 11:46:38 -0600

    Add files via upload

--- WheelofFortune/src/ConsoleUI.java
+++ WheelofFortune/src/ConsoleUI.java
@@ -1,4 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
 
-public class ConsoleUI {
+public class ConsoleUI extends Game {
 
+	
+	public static void main (String [] args) throws IOException {
+		Game g = new Game();
+		Puzzle p = new Puzzle();
+		
+		g.newGame();
+		g.importWheel();
+		g.setCurrent();
+		System.out.print("Welcome to Wheel of Fortune. Take a spin of the Wheel.");
+		g.spin();
+		Scanner read = new Scanner(System.in);
+		String input = read.next();
+		char a = read.next().charAt(0);
+		g.inputChar(a);
+			
+		
+	}
+	
+	
 }
