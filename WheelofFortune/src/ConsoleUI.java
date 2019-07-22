eb90fac9d17d94b144369c16e003074fc88756de
Richard Chow <richard.chow@ucalgary.ca>
2019-07-22 11:54:54 -0600

    Updates flow

--- WheelofFortune/src/ConsoleUI.java
+++ WheelofFortune/src/ConsoleUI.java
@@ -5,18 +5,34 @@ public class ConsoleUI extends Game {
 
 	
 	public static void main (String [] args) throws IOException {
+		String spoke = "";
 		Game g = new Game();
 		Puzzle p = new Puzzle();
+		Player p1 = new Player();
+		
+		
 		
 		g.newGame();
 		g.importWheel();
 		g.setCurrent();
 		System.out.print("Welcome to Wheel of Fortune. Take a spin of the Wheel.");
-		g.spin();
+		spoke = g.spin();
+		if (spoke == "loseaturn") {
+			loseATurn();
+		}
+
+		if (spoke == "bankrupt") {
+			bankrupt();
+		}
 		Scanner read = new Scanner(System.in);
 		String input = read.next();
 		char a = read.next().charAt(0);
 		g.inputChar(a);
+		//Check the answer
+		//If the answer is right, refresh()
+		//If the answer is wrong, changeTurn()
+		//If win, deposit value of wheel/get prize
+
 			
 		
 	}
