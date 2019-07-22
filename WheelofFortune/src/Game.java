b2cadeda9b62065ef0ea8a74a72eb41cb75acc0c
CyanDrudis <52932421+CyanDrudis@users.noreply.github.com>
2019-07-21 22:20:52 -0600

    Add files via upload

--- WheelofFortune/src/Game.java
+++ WheelofFortune/src/Game.java
@@ -4,10 +4,59 @@ public class Game
 {
 	private String puzzle;
 	private String answer;
-	private String current;
 	private int whosTurn = 0; //0 = player, 1 = AI, 2 = AI
 	private static ArrayList<String> wheel = new ArrayList<String>();
+	private static ArrayList<String> current = new ArrayList<String>();
+	private static ArrayList<String> guessed = new ArrayList<String>();
+	private char[] answers;
+	
 	Puzzle p = new Puzzle();
+	
+	ArrayList<Player> players = new ArrayList<Player>();
+	public void importWheel() throws IOException 
+	{
+		wheel = p.getWheel();
+	}
+	public void refresh() throws IOException 
+	{
+		char[] check;
+		check = p.getAnswer().toCharArray();
+		for(int i = 0; i < check.length; i++) 
+		{
+			if(check[i] == ' ') 
+			{
+				current.add(" ");	
+			}
+			else if(guessed.contains(check[i]))
+			{
+				current.set(i, check[i]+"");
+			}
+			else
+			{
+				current.add("*");
+			}
+		}
+	}
+	public void newGame() throws IOException 
+	{
+		answer = p.getAnswer();
+		p.newAnswer();
+		p.newPuzzle();
+		Player main = new Player();
+		Player AI1 = new Player();
+		Player AI2 = new Player();
+		players.add(main);
+		players.add(AI1);
+		players.add(AI2);
+		answers = p.getAnswer().toCharArray();
+	}
+	public void newPuzzle() throws IOException 
+	{
+		answers = p.getAnswer().toCharArray();
+		answer = p.getAnswer();
+		p.newAnswer();
+		p.newPuzzle();
+	}
 	public void bankrupt(Player p) 
 	{
 		p.setMoney(0.00);
@@ -25,40 +74,50 @@ public class Game
 	}
 	public void setCurrent() throws IOException 
 	{
-		
+		char[] check;
+		check = p.getAnswer().toCharArray();
+		for(int i = 0; i < check.length; i++) 
+		{
+			if(check[i] == ' ') 
+			{
+				current.add(" ");	
+			}
+			else
+			{
+				current.add("*");
+			}
+		}
 	}
-	public boolean freePlay() 
+	public void freePlay(char a) 
 	{
-		return true;
+		int numOfVowels = 0;
+		for(int i = 0; i < current.size(); i++) 
+		{
+			if(a == answers[i]) 
+			{
+				numOfVowels++;
+			}
+		}
+		players.get(whosTurn).pricePerVowel(numOfVowels, 0);
 	}
 	public void spin() 
 	{
 		
 	}
-	public boolean checkAns() 
+	public boolean checkAns(String a) throws IOException 
 	{
-		return true;
+		if(a.toLowerCase() == p.getAnswer().toLowerCase()) 
+		{
+			return true;
+		}
+		return false;
 	}
-	public int inputChar(char a) 
+	public void inputChar(char a) 
 	{
-		return 1;
+		guessed.add(a+"");
 	}
 	public boolean win() 
 	{
 		return false;
-	}
-	public String board() 
-	{
-		return "this will include which chars have been guessed out of the answer so far";
-	}
-	public static void main(String[]args) throws IOException 
-	{
-		Puzzle p = new Puzzle();
-		wheel = p.getWheel();
-		//(below)tests for wheel being imported
-		for(int i = 0; i < wheel.size(); i++) 
-		{
-			System.out.println(wheel.get(i));
-		}
 	}
 }
