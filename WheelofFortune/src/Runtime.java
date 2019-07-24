dc65e7d4fe822a4c9afa4b922f9745df6b1cf5b9
CyanDrudis <52932421+CyanDrudis@users.noreply.github.com>
2019-07-24 11:21:54 -0600

    Add files via upload

--- WheelofFortune/src/Runtime.java
+++ WheelofFortune/src/Runtime.java
@@ -14,31 +14,55 @@ import java.util.Scanner;
 public class Runtime extends Game {
 		
 		
-		String[] spoke;
+		String spoke;
 		Game g = new Game();
 		Puzzle p = new Puzzle();
 		//Player p1 = new Player();
 		//Player p2 = new Player();
 		
-		public void main(String[] args) throws IOException {
-		
-		g.newGame();
-		g.importWheel();
-		g.setCurrent();
-		System.out.print("Welcome to Wheel of Fortune. Take a spin of the Wheel.");
-		spoke = g.spin().split(",");
-		if (spoke[1] == "loseaturn") {
-			loseATurn();
-		}
-
-		if (spoke[1] == "bankrupt") {
-			bankrupt();
-		}
-		Scanner read = new Scanner(System.in);
-		String input = read.next();
-		char a = read.next().charAt(0);
-		g.inputChar(a); 
-		}
+	public void main(String[] args) throws IOException {
+		while(win()==false) {
+			System.out.println("New puzzle!");
+			while(winPuzzle()==false) {
+				g.newGame();
+				g.importWheel();
+				g.setCurrent();
+				System.out.println("Welcome to Wheel of Fortune. Take a spin of the Wheel.");
+				spoke = g.spin();
+				System.out.println("You spun the wheel and landed on" + spoke);
+				if (spoke == "loseaturn") {
+					loseATurn();
+					System.out.println("Sorry you lose a turn!");
+				}
+				if (spoke == "bankrupt") {
+					bankrupt();
+					System.out.println("Oh no you went bankrupt!");
+				}
+				System.out.println("Puzzle:" + g.getCurrent());
+				System.out.println("Now guess a letter or try to solve :");
+				Scanner read = new Scanner(System.in);
+				String input = read.nextLine();
+				char check [] = input.toCharArray();
+				if(check.length == 1) 
+				{
+					char a = check[0];
+					if(inputChar(a)) 
+					{
+						System.out.println("Correct!");
+					}
+					refresh();
+				}
+				else
+				{
+					if(g.checkAns(input)) 
+					{
+						System.out.println("Correct!");
+					}
+					refresh();
+					g.newGame();
+				}
+		}	}
+	}
 		//Check the answer
 		//If the answer is right, refresh()
 		//If the answer is wrong, changeTurn()
