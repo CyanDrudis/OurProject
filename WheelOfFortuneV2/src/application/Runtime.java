ad62e2ee8b1869667a493efd4dc2b78411ab245b
Richard <51135299+rzwc@users.noreply.github.com>
2019-08-04 22:20:04 -0600

    Fix indentation

--- WheelOfFortuneV2/src/application/Runtime.java
+++ WheelOfFortuneV2/src/application/Runtime.java
@@ -30,67 +30,67 @@ public class Runtime extends Game {
             g.winPuzzleCounter();
 			System.out.println("New puzzle!");
 			while(!g.winPuzzle()) {
-                            spoke = g.spin();
-                            while(spoke.equals("freespin")){
-                                System.out.println("You spun the wheel and... Free Spin! Spinning again...");
-                                spoke = g.spin();
-                            }
+				spoke = g.spin();
+				while(spoke.equals("freespin")){
+					System.out.println("You spun the wheel and... Free Spin! Spinning again...");
+					spoke = g.spin();
+				}
                 int playersTurn = g.whosTurn() + 1;
-            if (spoke.equals("loseaturn")) {
-            	g.loseATurn();
-				System.out.println("You spun the wheel and... Sorry you lose a turn!");
-                playersTurn = g.whosTurn() + 1;
-                System.out.println("Player "+ playersTurn +" next!");
-            } else {
-            	 if (spoke.equals("bankrupt")) {
-                 g.bankrupt();
-                 System.out.println("You spun the wheel and... Oh no you went bankrupt!" );
-                 playersTurn = g.whosTurn() + 1;
-                 System.out.println("Player "+ playersTurn +" next!");
-            	 } else {
-                     System.out.println("You spun the wheel and landed on $" + spoke);
-                     System.out.println("Player " + playersTurn +" Turn" + " | Category: " + g.getPuzzle() + " | Balance: " + g.getBal());
-                     System.out.println("Puzzle: " + g.getCurrent());
-                     System.out.println("Now guess a letter or try to solve :");
-                     Scanner read = new Scanner(System.in);
-                     String input = read.nextLine();
-                     char check [] = input.toCharArray();
-                     
-                     if(input.equals("Quit")) {
-                    	 System.exit(0);
-                     } else if (input.equals("quit")){
-                    	 System.exit(0);
-                     }
-                     
-                     if (check.length == 1) {
-                     	char a = check[0];
-                    	String outcome = g.inputChar(a);
-                     if(outcome.equals("there")) {
-                     	System.out.println("Correct!");
-                     } else if (outcome.equals("notThere")){
-                    	 System.out.println("Letter is not in puzzle!");
-                     	 g.changeTurn();
-                     } else if (outcome.equals("alreadyThere")) {
-                    	 System.out.println("That letter has already been guessed!");
-                         g.changeTurn();
-                     } else if (outcome.equals("puzzleComplete")){
-                         playersTurn = g.whosTurn() + 1;
-                         System.out.println("Correct! Player "+ playersTurn + " solved the puzzle");
-                     }
-                     //g.refresh();
-                     } else {
-                         if (g.checkAns(input)) {
-                             playersTurn = g.whosTurn() + 1;
-                         	System.out.println("Correct! Player "+ playersTurn + " solved the puzzle");
-                         } else {
-                        	 System.out.println("Incorrect!");
-                           	 g.changeTurn();
-                         }
-                         //g.refresh();
-                         //g.newGame();
-                     }
-            	 }
-            }
+				if (spoke.equals("loseaturn")) {
+					g.loseATurn();
+					System.out.println("You spun the wheel and... Sorry you lose a turn!");
+					playersTurn = g.whosTurn() + 1;
+					System.out.println("Player "+ playersTurn +" next!");
+				} else {
+					 if (spoke.equals("bankrupt")) {
+					 g.bankrupt();
+					 System.out.println("You spun the wheel and... Oh no you went bankrupt!" );
+					 playersTurn = g.whosTurn() + 1;
+					 System.out.println("Player "+ playersTurn +" next!");
+					 } else {
+						 System.out.println("You spun the wheel and landed on $" + spoke);
+						 System.out.println("Player " + playersTurn +" Turn" + " | Category: " + g.getPuzzle() + " | Balance: " + g.getBal());
+						 System.out.println("Puzzle: " + g.getCurrent());
+						 System.out.println("Now guess a letter or try to solve :");
+						 Scanner read = new Scanner(System.in);
+						 String input = read.nextLine();
+						 char check [] = input.toCharArray();
+						 
+						 if(input.equals("Quit")) {
+							 System.exit(0);
+						 } else if (input.equals("quit")){
+							 System.exit(0);
+						 }
+						 
+						 if (check.length == 1) {
+							char a = check[0];
+							String outcome = g.inputChar(a);
+						 if(outcome.equals("there")) {
+							System.out.println("Correct!");
+						 } else if (outcome.equals("notThere")){
+							 System.out.println("Letter is not in puzzle!");
+							 g.changeTurn();
+						 } else if (outcome.equals("alreadyThere")) {
+							 System.out.println("That letter has already been guessed!");
+							 g.changeTurn();
+						 } else if (outcome.equals("puzzleComplete")){
+							 playersTurn = g.whosTurn() + 1;
+							 System.out.println("Correct! Player "+ playersTurn + " solved the puzzle");
+						 }
+						 //g.refresh();
+						 } else {
+							 if (g.checkAns(input)) {
+								 playersTurn = g.whosTurn() + 1;
+								System.out.println("Correct! Player "+ playersTurn + " solved the puzzle");
+							 } else {
+								 System.out.println("Incorrect!");
+								 g.changeTurn();
+							 }
+							 //g.refresh();
+							 //g.newGame();
+						 }
+					 }
+				}
 			}
 			int playersTurn = g.whosTurn() + 1;
             System.out.println("Congratulations player "+ playersTurn + " you successfully solved the puzzle!");
