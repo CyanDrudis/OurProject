2e74606727e2b08825958aefef1d2a78099be060
Richard <51135299+rzwc@users.noreply.github.com>
2019-08-06 18:27:47 -0600

    Updated indentation and javadoc comments

--- WheelOfFortuneV2/src/application/Game.java
+++ WheelOfFortuneV2/src/application/Game.java
@@ -53,172 +53,158 @@ public class Game {
 	private static final int numberOfPuzzlesToWin = 3;
 	private int numberOfPuzzles = 0;
     private int randomNumForWheel;
-    private boolean twoPlayerMode = false;
 	Puzzle p = new Puzzle();
 	ArrayList<String> wheel = new ArrayList<String>();
 	ArrayList<Player> players = new ArrayList<Player>();
 	double[] money = {0.0, 0.0, 0.0};
-	/***************************************************************************************
-	 * FUNCTION: setTwoPlayerMode
+	
+	/**
 	 * 
-	 * DESCRIPTION: sets the game to two player mode
+	 * This method is used to deposit an amount in the players 
+	 * temporary account, which player receives the money depends 
+	 * on who's turn it is, signified by the whosTurn instance variable. 
+	 * This temporary account's balance will then be transferred
+	 * over to a permanent account that will keep the total balance 
+	 * which will be used to substatiate the winner of the game.
 	 * 
+	 * @param amount the amount of money that will be deposited into the 
+	 * players account
 	 * 
-	 * METHOD: setTwoPlayerMode()
-	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: bool : boolean
-	 * 
-	 *********************************************************************************************/
-	public void setTwoPlayerMode(boolean bool) {
-		twoPlayerMode = bool;
-	}
-	/***************************************************************************************
-	 * FUNCTION: getTwoPlayerMode
-	 * 
-	 * DESCRIPTION: checks to see if we're in two player mode
+	 * @return void
 	 * 
 	 * 
-	 * METHOD: getTwoPlayerMode()
-	 * 
-	 * RETURNS: boolean
-	 *
-	 * INPUT PARAMETERS: none
-	 * 
-	 *********************************************************************************************/
-	public boolean getTwoPlayerMode() {
-		return twoPlayerMode;
-	}
-	/***************************************************************************************
-	 * FUNCTION: Deposit
-	 * 
-	 * DESCRIPTION: deposit to the players temporary account
-	 * 
-	 * 
-	 * METHOD: deposit()
-	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: amount : double
-	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public void deposit(double amount) {
 		money[whosTurn] += amount;
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Deposit
+	/**
 	 * 
-	 * DESCRIPTION: deposit to the players temporary account
+	 * This method is used to withdraw an amount from the players temporary 
+	 * account, which player has the money withdrawn depends on who's turn 
+	 * it is, signified by the whosTurn instance variable. This temporary 
+	 * account's balance will then be transferred over to a permanent 
+	 * account that will keep the total balance which will be used to
+	 * substatiate the winner of the game.
+	 * 
+	 * @param amount the amount of money that will be withdrawn from the 
+	 * players account
+	 * 
+	 * @return void
 	 * 
 	 * 
-	 * METHOD: deposit()
-	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: amount : double
-	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public void withdraw(double amount) {
 		money[whosTurn] -= amount;
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: getRandomNumForWheel
+	/**
 	 * 
-	 * DESCRIPTION: returns the random number generated for the wheel
+	 * This method is used to get a random number for the wheel, this is 
+	 * used to ensure that there is no specific order of the values that 
+	 * the wheel can return. The values that the wheels returns from is in 
+	 * a text file called wheel.txt. 
 	 * 
-	 * METHOD: getRandomNumForWheel()
+	 * @param none
 	 * 
-	 * RETURNS: int
-	 *
-	 * INPUT PARAMETERS: none
+	 * @returns integer that's used as an index to get a certain value of 
+	 * the wheel from the text
+	 * file. 
 	 * 
-	 *********************************************************************************************/
+	 * 
+	 */
+	
     public int getRandomNumForWheel(){
         return randomNumForWheel;
     }
     
-	/***************************************************************************************
-	 * FUNCTION: Importing the wheel
+	/**
+	 * 	 
+	 * This method is used to import the wheel, it calls on a method that's
+	 * found in the puzzle class, which takes all of the values found in 
+	 * the wheel.txt file and imports them all into an array. 
 	 * 
-	 * DESCRIPTION: importing the wheel from a text file containing all the wheel's 
-	 * options using the function in the puzzle class
+	 * @param none
 	 * 
-	 * METHOD: importWheel()
+	 * @returns none
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void
+	 * @throws If an input or output exception occurred
 	 * 
-	 *********************************************************************************************/
+	 * 
+	 */
+    
 	public void importWheel() throws IOException {
 		wheel = p.getWheel();
 	}
-	/***************************************************************************************
-	 * FUNCTION: 
+	
+	/**
 	 * 
-	 * DESCRIPTION: 
+	 * This method returns the puzzle that represents the category of puzzle
+	 * that the user will be guessing from, found from the puzzle text file
+	 * 
+	 * @param none
+	 * 
+	 * @returns a puzzle of type String that represents the category of puzzle
+	 * that the user will be getting the String that they will have to guess
 	 * 
 	 * 
-	 * METHOD: getPuzzle() 
-	 * 
-	 * RETURNS: 
-	 *
-	 * INPUT PARAMETERS:
-	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public String getPuzzle() {
 		return puzzle;
 	}
-	/***************************************************************************************
-	 * FUNCTION: Get the balance of the player
+	
+	/**
 	 * 
-	 * DESCRIPTION: 
+	 * This method retrieves the balance of the player who's turn it currently
+	 * is on, it calls on the getMoney function that's implemented in the same
+	 * Game class. 
+	 *  
+	 * @param none
+	 * 
+	 * @returns the balance of the player who's turn it current is
 	 * 
 	 * 
-	 * METHOD: getBal()
-	 * 
-	 * RETURNS: 
-	 *
-	 * INPUT PARAMETERS:
-	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public double getBal() {
 		return players.get(whosTurn).getMoney();
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Setting the balance of the player
+	/**
 	 * 
-	 * DESCRIPTION: 
-	 * 
-	 * 
-	 * METHOD: getBal()
-	 * 
-	 * RETURNS: 
+	 * This method sets the balance of a player, it can set the balance
+	 * of any player regardless of return and assigns a balance to it
 	 *
-	 * INPUT PARAMETERS:
+	 * @param whosTurn an integer which indicates whos turn it is in order
+	 * to set the balance for the correct player
 	 * 
-	 *********************************************************************************************/
+	 * @param money the amount of money that will be set for that specific
+	 * player
+	 * 
+	 * @returns void
+	 * 
+	 * 
+	 */
+	
 	public void setBal(int whosTurn, int money) {
 		players.get(whosTurn).setMoney(money);
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Getting highest balance
-	 * 
-	 * DESCRIPTION: 
-	 * 
-	 * 
-	 * METHOD: getHighestBalPlayer()
-	 * 
-	 * RETURNS: 
-	 *
-	 * INPUT PARAMETERS:
-	 * 
-	 *********************************************************************************************/
+	 /** 
+	  *
+	  * This method sets the balance of a player, it can set the balance
+	  * of any player regardless of return and assigns a balance to it
+	  *
+	  * @param none
+	  * 
+	  * @returns void
+	  * 
+	  * 
+	  */ 
+	
 	public int getHighestBalPlayer() {
 		int highest = 0;
 		double highestBal = 0.0;
@@ -231,63 +217,77 @@ public class Game {
 		return highest;
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Getting player list size
-	 * 
-	 * DESCRIPTION: 
-	 * 
-	 * 
-	 * METHOD: getPlayerListSize()
-	 * 
-	 * RETURNS: 
-	 *
-	 * INPUT PARAMETERS:
-	 * 
-	 *********************************************************************************************/
+	 /** 
+	  *
+	  * This retrieves the player list size stored in the PlayerList array,
+	  * this is useful so when the logic is implemented to allow a set amount
+	  * of players to play in the game, and when the game resets, a loop
+	  * will be performed where the players balance is set to 0, therefore,
+	  * needing the player list size to loop through all the indices
+	  *
+	  * @param none
+	  * 
+	  * @returns an integer that represents the size of the PlayerList array
+	  * 
+	  * 
+	  */ 
+	
 	public int getPlayerListSize() {
 		return players.size();
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: creating a brand new game (puzzle and answer)
-	 * 
-	 * DESCRIPTION: creates a brand new game for the player to try and win, chooses a puzzle,
-	 * then chooses an answer from that puzzle category, creates three player accounts
-	 * 
-	 * METHOD: newGame()
-	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void  
-	 * 
-	 *********************************************************************************************/
+	/** 
+	  *
+	  * This method creates a brand new game first by creating a new Puzzle(),
+	  * which is the shell of the game, the guessed array is then cleared, 
+	  * a new puzzle category is retrieved using the getPuzzle() method, then
+	  * the answer is retrieved using the getAnswer() method, according to the 
+	  * category. The string answer is then converted into a character array 
+	  * which will then be used to compare the guesses towards. The default 
+	  * amount of players is three, therefore, three instances of player are
+	  * created, and added to an array list for players
+	  * 
+	  * @param void
+	  * 
+	  * @returns void
+	  * 
+	  * @throws If an input or output exception occurred
+	  * 
+	  * 
+	  */ 
+	
 	public void newGame() throws IOException {
-		p = new Puzzle();
-		guessed.clear();
-		puzzle = p.getPuzzle();
-		answer = p.getAnswer();
-		answers = answer.toCharArray();
-		Player p1 = new Player();
-		Player p2 = new Player();
-		Player p3 = new Player();
-		players = new ArrayList<Player>();
-		players.add(p1);
-		players.add(p2);
-		players.add(p3);
+	p = new Puzzle();
+	guessed.clear();
+	puzzle = p.getPuzzle();
+	answer = p.getAnswer();
+	answers = answer.toCharArray();
+	Player p1 = new Player();
+	Player p2 = new Player();
+	Player p3 = new Player();
+	players = new ArrayList<Player>();
+	players.add(p1);
+	players.add(p2);
+	players.add(p3);
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Create a new puzzle and obtain an answer from that puzzle
+	/**
+	 * 	 
+	 * This method creates a new puzzle by retrieving the category from the
+	 * puzzle text file, as well, a corresponding answer will be picked from
+	 * the category. This puzzle is then transferred to a character array,
+	 * and every player gets their balance reset to 0.
 	 * 
-	 * DESCRIPTION: transfer answer to character array, create new answer and puzzle
+	 * @param none
 	 * 
-	 * METHOD: newPuzzle()
+	 * @returns none
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void
+	 * @throws If an input or output exception occurred
 	 * 
-	 *********************************************************************************************/
+	 * 
+	 */
+		
+		
 	public void newPuzzle() throws IOException {
 		p = new Puzzle();
 		puzzle = p.getPuzzle();
@@ -296,82 +296,87 @@ public class Game {
 		players.get(0).deposit(money[0]);
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Reset users money upon landing on bankrupt section of the wheel
+	/**
+	 * 	 
+	 * This method resets users money upon landing on bankrupt section of
+	 * the wheel, by setting the player's balance to 0. The player who's
+	 * balance will be reset will depedn on the whosTurn instance variable,
+	 * as they are the person who landed on it 
 	 * 
-	 * DESCRIPTION: sets player who lands on the bankrupt section's money to 0
+	 * @param none
 	 * 
-	 * METHOD: bankrupt()
+	 * @returns none
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void
 	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public void bankrupt() {
-		money[whosTurn] = 0;
-        changeTurn();
+	money[whosTurn] = 0;
+    changeTurn();
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Cycle through players turn	
+	/**
+	 * 	 
+	 * This method is used to increment a players turn as the player finishes
+	 * their turn, or in the scenario where they land on the loseaturn spoke
+	 * on the wheel, in which case, they lose their chance to guess any letters,
+	 * and their turn gets skipped, and passed onto the next person
 	 * 
-	 * DESCRIPTION: Cycle through players turn 	
+	 * @param none
 	 * 
-	 * METHOD: loseATurn()
+	 * @returns none
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void
 	 * 
-	 *********************************************************************************************/
+	 */
+	
 	public void loseATurn(){
-		if(twoPlayerMode) {
-			if(whosTurn == 1) {
-				whosTurn = 0;
-			} else {
-				whosTurn = 1;
-			}
-		}else if(whosTurn == 2) {
+		if(whosTurn == 2) {
 			whosTurn = 0;
 		}	else	{
 			whosTurn += 1;
 		}
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Set Turn
+	/**
+	 * 	 
+	 * This method sets the turn, and is used when...
 	 * 
-	 * DESCRIPTION: 
 	 * 
-	 * METHOD: setTurn()
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void
+	 * @param none
 	 * 
-	 *********************************************************************************************/
+	 * @returns none
+	 * 
+	 * 
+	 */
+	
 	public void setTurn(int turn){
-		whosTurn = turn;
+	whosTurn = turn;
 	}
 	
-	/***************************************************************************************
-	 * FUNCTION: Set the base string to which the user is going to guess characters to fill up the word 
+	/**
+	 * 	 
+	 * This method creates an empty array called check, that is then set to
+	 * a character array of answer, the current array is then cleared for 
+	 * the updated current character array. For each index of the answer, if
+	 * the check is equal to a space, there's a space added in the current 
+	 * array, if it's equivalent to any of the guessed letters regardless
+	 * of lower/uppercase guess, it's reaplaced in the current string, and
+	 * if not, the un-guessed letters have asterisks inserted
 	 * 
-	 * DESCRIPTION: Go through each index of the length of the answer, and adds either blank spaces or
-	 * asterisks 
+	 * @param none
 	 * 
-	 * METHOD: setCurent()
+	 * @returns none
 	 * 
-	 * RETURNS: void
-	 *
-	 * INPUT PARAMETERS: void  
+	 * @throws If an input or output exception occurred
 	 * 
-	 *********************************************************************************************/
+	 * 
+	 */
+	
 	public void setCurrent() throws IOException {
 		char[] check;
 		check = answer.toCharArray();
-                current.clear();
+        current.clear();
 		for(int i = 0; i < check.length; i++) {
 			if(check[i] == ' ') {
                             current.add(" ");	
@@ -383,26 +388,30 @@ public class Game {
 		}
 	}
 	
-        /***************************************************************************************
-	 * FUNCTION: 
+	/**
+	 * 	 
+	 * This method is used when the user inputs more than one character,
+	 * it assumes that what the user has put is the entire answer, 
+	 * this guess will then be compared to the answer, and will return
+	 * a boolean value
 	 * 
-	 * DESCRIPTION: 
+	 * @param strToCompare the String that's being compared
 	 * 
-	 * METHOD: containsCaseInsensitive(String strToCompare, ArrayList<String>list)
+	 * @param list 
 	 * 
-	 * RETURNS: 
-	 *
-	 * INPUT PARAMETERS:
+	 * @returns boolean, whether the user has guessed the answer correctly or
+	 * not 
 	 * 
-     * SOURCE: https://stackoverflow.com/questions/8751455/arraylist-contains-case-sensitivity
-	 *********************************************************************************************/
+	 * 
+	 */
+	
 	public boolean containsCaseInsensitive(String strToCompare, ArrayList<String>list) {
-         for(String str:list){
+		for(String str:list){
               if(str.equalsIgnoreCase(strToCompare)){
                   return(true);
               }
          }
-     return(false);
+		return(false);
      }
 	
 	/***************************************************************************************
@@ -527,13 +536,7 @@ public class Game {
 	 * 
 	 *********************************************************************************************/
 	public void changeTurn(){
-		if(twoPlayerMode) {
-			if(whosTurn == 1) {
-				whosTurn = 0;
-			} else {
-				whosTurn = 1;
-			}
-		}else if(whosTurn == 2) {
+		if(whosTurn == 2) {
 			whosTurn = 0;
 		}	else	{
 			whosTurn += 1;
@@ -580,20 +583,20 @@ public class Game {
 			if ((a+"").equalsIgnoreCase(answers[i]+"")) {
                             there = true;
 				guessed.add(a+"");
-                                if(a == 'a' ||a == 'e' ||a == 'i' ||a == 'o' || a =='u'){
-                                    money[whosTurn] -= vowelCost;
-                                }
-                                else{
-                                	money[whosTurn] += currentSpokeValue;
-                                }
+				if(a == 'a' ||a == 'e' ||a == 'i' ||a == 'o' || a =='u'){
+					money[whosTurn] -= vowelCost;
+				}
+				else{
+					money[whosTurn] += currentSpokeValue;
+				}
                 
 			}
 		}
-                setCurrent();
-                if(!current.contains("*")){
-                    guessed.clear(); 
-                    return "puzzleComplete";
-                }
+		setCurrent();
+		if(!current.contains("*")){
+			guessed.clear(); 
+			return "puzzleComplete";
+		}
         if(there){
         	return "there";
         }
@@ -712,7 +715,6 @@ public class Game {
 	        output.write("\n");
 	        output.write(whosTurn+"\n");
 	        output.write(getRandomNumForWheel()+"\n");
-	        output.write(twoPlayerMode+"");
 	        output.close();
         } catch ( IOException e ) {
             e.printStackTrace();
@@ -763,8 +765,6 @@ public class Game {
 			whosTurn = Integer.valueOf(line.trim());
 			line = br.readLine();
 			randomNumForWheel = Integer.valueOf(line.trim());
-			line = br.readLine();
-			twoPlayerMode = Boolean.valueOf(line.trim());
 			br.close();
         } catch ( IOException e ) {
             e.printStackTrace();
